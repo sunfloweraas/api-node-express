f9d96379bae3109a7c4d6aac32f38cee062810ef
Ana-Júlia <juhcristina1101@gmail.com>
2023-08-14 17:09:03 -0300

    alterações

--- src/server.js
+++ src/server.js
@@ -1,1 +1,17 @@
-console.log('rodando servidor...')
+import express from 'express'
+import userRouter from './routes/userRouter.js'
+import productRouter from './routes/productRouter.js'
+
+const api = express()
+
+api.get('/', (req, res) => {
+    res.json({ message: "Bem-vindo a nossa API" })
+})
+
+api.use('/user', userRouter)
+api.use('/product', productRouter)
+
+
+api.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000! http://localhost:3000')
+})
